5669eb1ed4c56e0fc873f35a057611e89335c6e3
lissalagrimas <lissalagrimas.5@gmail.com>
2025-12-11 21:36:40 +0800

    Add files via upload

---
+++ SIA-CRUD-3/error.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Error</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>.wrapper{ width: 600px; margin: 0 auto; }</style>
+</head>
+<body>
+<div class="wrapper">
+    <div class="container-fluid">
+        <h1 class="mt-5 mb-3">Invalid Request</h1>
+        <div class="alert alert-danger">
+            <p>Sorry, you have made an invalid request. Please <a href="index.php" class="alert-link">go back</a> and try again.</p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
